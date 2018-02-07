bac4551f4a0c85c04b8c938945e3c9e1773eb8a2
puffy <nagy.gergo@3pixel.hu>
2018-02-07 17:19:04 +0100

    electron and electron reload init

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+require('electron-reload')(__dirname);
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow();
+
+  mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
+
+  mainWindow.on('closed', () => mainWindow = null)
+}
+
+app.on('ready', createWindow);
