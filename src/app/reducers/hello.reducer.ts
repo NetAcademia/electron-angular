211f5b8c21982844031df35541a2a1726d69e90d
puffy <nagy.gergo@3pixel.hu>
2018-02-11 23:24:24 +0100

    hello vs post reducer hogy is ?

--- src/app/reducers/hello.reducer.ts
+++ src/app/reducers/hello.reducer.ts
@@ -1,17 +1,14 @@
 import {Action} from '@ngrx/store';
 
-const init = 'Szent habakukk Micimacko!';
+export function helloReducer(state: string, action: Action) {
+  console.log('helloReducer: ', action.type, state);
 
-export function helloReducer(state: string = init, action: Action) {
-
-    switch (action.type) {
-      case 'ENG':
-        return state = 'Hello world! Elon Musk RLZ';
-      case 'HUN':
-        return state = 'Hello vilag, jo nekunk mert angular';
-      case 'RESET':
-        return state = init;
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case 'ENG':
+      return state = 'Hello world! Elon Musk RLZ';
+    case 'HUN':
+      return state = 'Hello vilag, jo nekunk mert angular';
+    default:
+      return state = 'Szent habakukk Micimacko!';
+  }
 }
--- src/app/reducers/post.reducer.ts
+++ src/app/reducers/post.reducer.ts
@@ -2,7 +2,7 @@ import {GET_POST, GET_POST_SUCCES, PostActions} from '../actions/post.actions';
 import {Post} from '../models/post.model';
 
 export function postReducer(state: Post, action: PostActions) {
-  console.log(action.type, state);
+  console.log('postReducer: ', action.type, state);
 
   switch (action.type) {
     case GET_POST:
